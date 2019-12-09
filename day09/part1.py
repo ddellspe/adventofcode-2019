decf1b288e2f1de2998fec592541a258a210a943
David Dellsperger <david.dellsperger@gmail.com>
2019-12-09 17:37:36 -0600

    pushing day 9

--- day09/part1.py
+++ day09/part1.py
@@ -39,50 +39,43 @@ class Machine(object):
             m1 = op_code // 100 % 10
             m2 = op_code // 1000 % 10
             m3 = op_code // 10000 % 10
+            o1 = self.relative_location if m1 == 2 else 0
+            o2 = self.relative_location if m2 == 2 else 0
+            o3 = self.relative_location if m3 == 2 else 0
             p1, p2, p3 = self.memory[self.pointer + 1], self.memory[self.pointer + 2], self.memory[self.pointer + 3]
-            #print('op_code', op_code, p1, p2, p3, self.relative_location)
-            #print('modes', m1, m2, m3)
-            v1 = self.memory[p1] if m1 == 0 else (self.memory[self.relative_location + p1] if m1 == 2 else p1)
-            v2 = self.memory[p2] if m2 == 0 else (self.memory[self.relative_location + p2] if m2 == 2 else p2)
-            v3 = p3 if m3 == 0 and operation in {1, 2, 7, 8} else self.pointer + 3
-            #print('values', p1, p2, p3)
+            # print('op_code', op_code, p1, p2, p3, self.relative_location)
+            # print('modes', m1, m2, m3)
+            v1 = p1 if m1 == 1 else self.memory[o1+p1]
+            v2 = p2 if m2 == 1 else self.memory[o2+p2]
+            # print('values', v1, v2, v3)
             if operation == 99:
                 self.__halt = True
                 print('Halting')
                 break
             elif operation == 1:
-                print('adding', v1, 'and', v2, 'storing at', v3)
-                self.memory[v3] = v1 + v2
+                self.memory[o3+p3] = v1 + v2
                 self.pointer += 4
             elif operation == 2:
-                print('multiplying', v1, 'and', v2, 'storing at', v3)
-                self.memory[v3] = v1 * v2
+                self.memory[o3+p3] = v1 * v2
                 self.pointer += 4
             elif operation == 3:
-                print('taking value', input, 'storing at', v1)
-                self.memory[p1 + self.relative_location if m1 == 2 else 0] = input
+                self.memory[o1+p1] = input
                 self.pointer += 2
             elif operation == 4:
-                print('outputing value', v1)
                 self.out = v1
-                print(' ', self.out)
+                print(v1)
                 self.pointer += 2
             elif operation == 5:
-                print('moving pointer to', v2, 'if', v1, 'is not equal to 0')
                 self.pointer = v2 if v1 != 0 else self.pointer + 3
             elif operation == 6:
-                print('moving pointer to', v2, 'if', v1, 'is equal to 0')
                 self.pointer = v2 if v1 == 0 else self.pointer + 3
             elif operation == 7:
-                print('checking if', v1, 'is less than', v2, 'storing at', p3 + self.relative_location if m3 == 2 else 0)
-                self.memory[p3 + self.relative_location if m3 == 2 else 0] = 1 if v1 < v2 else 0
+                self.memory[o3+p3] = 1 if v1 < v2 else 0
                 self.pointer += 4
             elif operation == 8:
-                print('checking if', v1, 'is equal to', v2, 'storing at', p3 + self.relative_location if m3 == 2 else 0)
-                self.memory[p3 + self.relative_location if m3 == 2 else 0] = 1 if v1 == v2 else 0
+                self.memory[o3+p3] = 1 if v1 == v2 else 0
                 self.pointer += 4
             elif operation == 9:
-                print('moving relative_location from', self.relative_location, 'to', self.relative_location + v1)
                 self.relative_location += v1
                 self.pointer += 2
             else:
@@ -95,4 +88,3 @@ for l in open('input.txt', 'r'):
     mreset = [int(x) for x in l.strip().split(',')]
 m = Machine(mreset)
 m.input(1)
-#print(dict(m.memory_dict))
--- day09/part2.py
+++ day09/part2.py
@@ -1,12 +1,16 @@
+from collections import defaultdict
+
 import math
 import itertools
 
 class Machine(object):
     def __init__(self, mem):
-        self.memory = mem
+        self.memory = defaultdict(lambda: 0, enumerate(mem))
+        self.mem_len = len(mem)
         self.pointer = 0
         self.len = len(mem)
         self.out = 0
+        self.relative_location = 0
         self.__halt = False
         self.__initialized = False
         self.__error = False
@@ -30,76 +34,57 @@ class Machine(object):
         if input != None:
             self.__initialized = True
         while self.pointer < self.len:
-            if self.len - self.pointer < 5:
-                instr = self.memory + ([0] * 5)
-            else:
-                instr = self.memory[:self.pointer + 5]
             op_code = self.memory[self.pointer]
             operation = op_code % 100
             m1 = op_code // 100 % 10
             m2 = op_code // 1000 % 10
             m3 = op_code // 10000 % 10
-            p1, p2, p3 = instr[self.pointer + 1], instr[self.pointer + 2], instr[self.pointer + 3]
-            v1 = self.memory[p1] if m1 == 0 and operation != 99 else p1
-            v2 = self.memory[p2] if m2 == 0 and operation not in {99, 3, 4} else p2
-            v3 = p3 if m3 == 0 and operation in {1, 2, 7, 8} else self.pointer + 3
+            o1 = self.relative_location if m1 == 2 else 0
+            o2 = self.relative_location if m2 == 2 else 0
+            o3 = self.relative_location if m3 == 2 else 0
+            p1, p2, p3 = self.memory[self.pointer + 1], self.memory[self.pointer + 2], self.memory[self.pointer + 3]
+            # print('op_code', op_code, p1, p2, p3, self.relative_location)
+            # print('modes', m1, m2, m3)
+            v1 = p1 if m1 == 1 else self.memory[o1+p1]
+            v2 = p2 if m2 == 1 else self.memory[o2+p2]
+            # print('values', v1, v2, v3)
             if operation == 99:
                 self.__halt = True
+                print('Halting')
                 break
             elif operation == 1:
-                self.memory[v3] = v1 + v2
+                self.memory[o3+p3] = v1 + v2
                 self.pointer += 4
             elif operation == 2:
-                self.memory[v3] = v1 * v2
+                self.memory[o3+p3] = v1 * v2
                 self.pointer += 4
             elif operation == 3:
-                if input == None:
-                    break
-                self.memory[p1] = input
-                input = None
+                self.memory[o1+p1] = input
                 self.pointer += 2
             elif operation == 4:
                 self.out = v1
+                print(v1)
                 self.pointer += 2
             elif operation == 5:
                 self.pointer = v2 if v1 != 0 else self.pointer + 3
             elif operation == 6:
                 self.pointer = v2 if v1 == 0 else self.pointer + 3
             elif operation == 7:
-                self.memory[v3] = 1 if v1 < v2 else 0
+                self.memory[o3+p3] = 1 if v1 < v2 else 0
                 self.pointer += 4
             elif operation == 8:
-                self.memory[v3] = 1 if v1 == v2 else 0
+                self.memory[o3+p3] = 1 if v1 == v2 else 0
                 self.pointer += 4
+            elif operation == 9:
+                self.relative_location += v1
+                self.pointer += 2
             else:
-                print("ERROR", self.pointer, operation, self.memory[self.pointer:self.pointer + 4])
+                print("ERROR", self.pointer, operation, [op_code, p1, p2, p3])
                 self.__error = True
                 break
-def run(data, settings):
-    result, perms = 0, list(itertools.permutations(settings,5))
-    for p in perms:
-        m = [
-            Machine(data.copy()),
-            Machine(data.copy()),
-            Machine(data.copy()),
-            Machine(data.copy()),
-            Machine(data.copy())
-        ]
-
-        current, prev = 0, 4
-        while True:
-            if m[current].halted() or m[current].errored():
-                break
-            m[current].run(p[current] if not m[current].initialized() else m[prev].output())
-            prev = current
-            current = (current + 1) % 5
-        result = m[4].output() if m[4].output() > result else result
-    return result
 
 mreset = []
 for l in open('input.txt', 'r'):
     mreset = [int(x) for x in l.strip().split(',')]
-
-settings = list(range(5, 10))
-max = run(mreset, settings)
-print("Maximum Thrust: ", max)
+m = Machine(mreset)
+m.input(2)
